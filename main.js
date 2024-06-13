02e9c8b76a62b24d86e569ba642295dfb988db1f
NicholasBuiatti <nic.buiatti@gmail.com>
2024-06-13 16:42:42 +0200

    chiamata axios e verifica di successo

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            toDoList: [],
         }
     },
     mounted() {
         console.log("prova VUE");
+
+        axios.get("./server.php").then(results => {
+
+            this.toDoList = results.data;
+        });
     }
 }).mount('#app')
--- server.php
+++ server.php
@@ -14,3 +14,7 @@ $toDoList = [
         'chacked' => 'false'
     ],
 ];
+
+header('Content-Type: application/json');
+// TRASFORMO IL CONTENUTO IN UN FILE STRINGA CHE SI RIESCE A LEGGERE ANCHE IN ALTRI LINGUAGGI
+echo json_encode($toDoList);
