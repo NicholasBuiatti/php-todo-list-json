58fb7b8d7c29b338387010e2cc30a45f1dbebb0b
NicholasBuiatti <nic.buiatti@gmail.com>
2024-06-13 16:36:01 +0200

    creazione file con importazione e verifica di bootstrap vue e axios

---
+++ main.js
@@ -0,0 +1,11 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+        }
+    },
+    mounted() {
+        console.log("prova VUE");
+    }
+}).mount('#app')
---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+
+$toDoList = [
+    [
+        'toDo' => 'Fare la spesa',
+        'chacked' => 'true'
+    ],
+    [
+        'toDo' => 'Pulire Casa',
+        'chacked' => 'true'
+    ],
+    [
+        'toDo' => 'Rifare il letto',
+        'chacked' => 'false'
+    ],
+];
